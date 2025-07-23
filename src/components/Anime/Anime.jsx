d14dbd3d61229f7ef72861a2e16c9e8c39ab7829
harshithaendreddy <endreddyharshithareddy@outlook.com>
2025-07-23 14:22:58 +0530

    Feature: Add search functionality with Jikan API integration

--- src/components/Anime/Anime.jsx
+++ src/components/Anime/Anime.jsx
@@ -3,11 +3,16 @@ import Banner from './Banner.jsx'
 import axios from 'axios'
 import AnimeCard from './AnimeCard.jsx';
 import Pagination from '../Pagination.jsx';
+import { useSearchParams } from 'react-router-dom';
 
 function Anime() {
   const [anime, setAnime] = useState([])
   const [pageNo,setPageNo]= useState(1)
   const [watchList,setWatchList]=useState( JSON.parse(localStorage.getItem('watchlist')) || []);
+
+  const [searchParams] = useSearchParams()
+  const searchQuery = searchParams.get('search')
+
 
   const handleAddToWatchList = (animeObj)=>{
     const newWatchList = [...watchList,animeObj]
@@ -39,33 +44,36 @@ function Anime() {
 
   useEffect(() => {
     async function fetchData() {
-      const api_URL= import.meta.env.VITE_API_URL
-      
       try {
-        const res = await axios.get(`${api_URL}${pageNo}`);
-        setAnime(res.data.data)
-        // console.log(res.data)
+        if (searchQuery) {
+          const res = await axios.get(`${import.meta.env.VITE_JIKAN_API}anime?q=${searchQuery}`);
+          setAnime(res.data.data)
+        } else {
+          const api_URL = import.meta.env.VITE_API_URL
+          const res = await axios.get(`${api_URL}${pageNo}`)
+          setAnime(res.data.data)
+        }
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error)
       }
     }
     fetchData();
     console.log(watchList);
-  }, [pageNo]);
+  }, [pageNo, searchQuery]);
 
   return (
     <div>
       <Banner/>
       <div>
         <div className='w-full text-3xl text-center p-5 m-4'>
-          Trending Anime
+          {searchQuery ? `Search Results for "${searchQuery}"` : 'Trending Anime'}
         </div>
         <div className='flex flex-row flex-wrap justify-around gap-10'>
           {anime.map((animeObj) => (
             <AnimeCard key={animeObj.mal_id} watchList={watchList} animeObj={animeObj} image_url={animeObj.images.jpg.image_url} title={animeObj.title_english} handleAddToWatchList={handleAddToWatchList} handleRemoveFromWatchList={handleRemoveFromWatchList}/>
           ))}
         </div>
-        <Pagination pageNo={pageNo} handlePrev={handlePrev} handleNext={handleNext}/>
+         {!searchQuery && ( <Pagination pageNo={pageNo} handlePrev={handlePrev} handleNext={handleNext}/>)}
       </div>
     </div>
   )
