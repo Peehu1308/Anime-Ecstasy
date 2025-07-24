2d5d888c8d3662558cebc60eeb1ca610928fedc3
Peehu Mishra <peehumishra1308@gmail.com>
2025-07-25 02:08:03 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "anime-neon":
+          "linear-gradient(135deg, #1B1F3B, #6C0BA9, #FF4E9B, #00F0FF)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
