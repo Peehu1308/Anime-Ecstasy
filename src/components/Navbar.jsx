1f58719f24f2b78be42b47550fe0758241cf0c2c
Peehu Mishra <peehumishra1308@gmail.com>
2025-07-25 02:18:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
-        <nav className='flex space-x-8 w-full items-center p-4 bg-black mx-4 justify-center'>
+        <div className='flex flex-row w-full bg-black'>
+            <nav className='flex space-x-8 w-full items-center p-4 bg-black mx-4 justify-center'>
             {/* <img src='/logo.svg' alt="logo" className='w-[50px]' /> */}
             <div className="text-3xl fixed left-3 top-3 font-extrabold bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-400 bg-clip-text text-transparent">
   Anime Ecstasy
@@ -16,5 +17,11 @@ export const Navbar = () => {
             <NavLink to={"/watchlist"} className='text-xl font-bold text-white hover:text-pink-300'>Watch-list</NavLink>
 
         </nav>
+        <div className='flex items-end'>
+            <button className='bg-pink-500 rounded-md p-2 w-[7rem] m-2 hover:bg-yellow-500 hover:text-[black]'>Sign-up</button>
+        </div>
+        </div>
+        
+        
     )
 }
